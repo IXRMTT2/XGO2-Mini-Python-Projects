17f05dbf452a228c93d34a6a13d6cfc06169da33
IXRMTT <ixrgithub@gmail.com>
2025-03-12 13:30:58 +0000

    updated spin

--- Basic Movements/spin.py
+++ Basic Movements/spin.py
@@ -4,13 +4,13 @@ import time
 def spin():
     robot = XGO()
 
-    robot.turn(direction=1, speed=50) # Change direction to 0 for anti-clockwise
+    robot.turn(direction=1, speed=50)
     print("Spinning")
 
-    time.sleep(5) # Change this to control how long he will spin for
+    time.sleep(5) # change how long he will spin for
 
     robot.stop()
-    print("Stopped spinning")
+    print("Stopped")
 
 if __name__ == "__main__":
     spin()
