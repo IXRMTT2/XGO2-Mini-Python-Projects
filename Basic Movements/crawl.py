7edb48f86702ca75b679ff1c0f2e4e1331e934ad
IXRMTT <ixrgithub@gmail.com>
2025-03-13 10:16:43 +0000

    updates crawl

--- Basic Movements/crawl.py
+++ Basic Movements/crawl.py
@@ -2,18 +2,14 @@ import time
 from xgoedu import XGOEDU
 from xgolib import XGO
 
-
 XGO_edu = XGOEDU()
 dog = XGO('xgomini')
-
-
 def crawl():
     print("Starting to crawl...")
     
     dog.lower_body()  
     time.sleep(1)
-    
-
+ 
     dog.walk(direction=0, speed=50)  # Move forward at walking speed
     time.sleep(3)  # Crawl for 3 seconds
     dog.stop()  # Stop the robot
